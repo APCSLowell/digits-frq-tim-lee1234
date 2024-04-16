668167bf902ff76ce81fe70cb9984e969ab45de9
tim-lee1234 <143123845+tim-lee1234@users.noreply.github.com>
2024-04-15 22:18:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,13 +5,23 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	public Digits(int num){
+	digitList = new ArrayList<Integer>;
+	if (num == 0)
+	digitList.add (new Integer (0));
+	while (num >0)
+	digitList.add (new Integer(num%10));
+	num/=10;
+	}
+	
 	}
 
-	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
+	public boolean isStrictlyIncreasing(){
+	for (int i = 0; i < digitList.size()-1; i++){
+		if (digitList.get(i).intValue() >= digitList.get(i+1).intValue())
+			return false;
+	}
+	return true; 
 		
 
 	}
